3f1433b2e093d131a1043ad84654da01ebfce250
O2 <167840668+oxygen-batd@users.noreply.github.com>
2024-06-25 10:47:19 +0700

    update image preprocessing

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from paddleocr import PaddleOCR
 import cv2
+import numpy as np
 
 ocr = PaddleOCR(
     use_angle_cls=True,
@@ -8,15 +9,22 @@ ocr = PaddleOCR(
     det_algorithm='DB',
     rec_algorithm='SVTR_LCNet')
 
-def read_license_plate(license_plate_crop, zoom_factor=10):
+def image_preprocessing(image):
+    w,h = image.shape[:2]
+    if h < 80:
+        image = cv2.resize(image, (300, 300), fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    
+    # PaddleOCR requires RGB
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+
+def read_license_plate(license_plate_crop):
     try:
-        zoomed_crop = cv2.resize(license_plate_crop, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_LINEAR)
-        
-        # Convert license plate crop to RGB (PaddleOCR requires RGB)
-        license_plate_crop_rgb = cv2.cvtColor(zoomed_crop, cv2.COLOR_BGR2RGB)
-        
-        # Perform OCR
-        results = ocr.ocr(license_plate_crop_rgb, cls=True)
+        image = image_preprocessing(license_plate_crop)
+        results = ocr.ocr(image, cls=True)
         
         all_texts = []
         for res in results:
@@ -38,3 +46,19 @@ def read_license_plate(license_plate_crop, zoom_factor=10):
     except Exception as e:
         print(f"Error in reading license plate: {e}")
         return None
+
+def limit_text_within_image(image, text, org, font, font_scale, thickness):
+    text_size, _ = cv2.getTextSize(text, font, font_scale, thickness)
+    text_width, text_height = text_size
+
+    org_x, org_y = org
+    if org_x < 0:
+        org_x = 0
+    if org_y < text_height:
+        org_y = text_height
+    if org_x + text_width > image.shape[1]:
+        org_x = image.shape[1] - text_width - 5
+    if org_y - text_height < 0:
+        org_y = text_height
+
+    return (org_x, org_y)
